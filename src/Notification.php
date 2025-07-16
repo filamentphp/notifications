9b9022c6f682755317092b289a61928348310737
Dan <git@danharrin.com>
2025-07-16 13:41:20 +0000

    Merge pull request #16979 from skaesdorf/fix/typo-in-date-time-picker-blade

--- src/Notification.php
+++ src/Notification.php
@@ -9,6 +9,7 @@ use Filament\Notifications\Events\DatabaseNotificationsSent;
 use Filament\Notifications\Livewire\Notifications;
 use Filament\Notifications\View\Components\NotificationComponent;
 use Filament\Notifications\View\Components\NotificationComponent\IconComponent;
+use Filament\Notifications\View\NotificationsIconAlias;
 use Filament\Support\Components\Contracts\HasEmbeddedView;
 use Filament\Support\Components\ViewComponent;
 use Filament\Support\Concerns\HasColor;
@@ -392,7 +393,7 @@ class Notification extends ViewComponent implements Arrayable, HasEmbeddedView
                 x-on:click="close"
                 class="fi-icon-btn fi-no-notification-close-btn"
             >
-                <?= generate_icon_html(Heroicon::XMark, alias: 'notifications::notification.close-button')->toHtml() ?>
+                <?= generate_icon_html(Heroicon::XMark, alias: NotificationsIconAlias::NOTIFICATION_CLOSE_BUTTON)->toHtml() ?>
             </button>
         </div>
 

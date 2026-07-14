f993d3d7385331668f985c138f1b669ddd6091e6
Dan Harrin <git@danharrin.com>
2026-07-14 20:31:38 +0000

    fix: More a11y issues (#20168)

--- resources/views/database-notifications.blade.php
+++ resources/views/database-notifications.blade.php
@@ -11,10 +11,18 @@
 @endphp
 
 <div class="fi-no-database">
+    {{-- The focus trap autofocuses the modal window itself when the slide-over opens, since the first tabbable element is the `Mark all as read` header action, which `Enter` would otherwise immediately (and irreversibly) trigger. The window must carry the `autofocus` attribute because the focus trap resolves it once, when the modal first initializes, and the window is always rendered. --}}
     <x-filament::modal
         :alignment="$hasNotifications ? null : Alignment::Center"
+        aria-labelledby="database-notifications.heading"
         close-button
         :description="$hasNotifications ? null : __('filament-notifications::database.modal.empty.description')"
+        :extra-modal-window-attribute-bag="
+            new \Filament\Support\View\ComponentAttributeBag([
+                'autofocus' => true,
+                'tabindex' => '-1',
+            ])
+        "
         :heading="$hasNotifications ? null : __('filament-notifications::database.modal.empty.heading')"
         :icon="$hasNotifications ? null : \Filament\Support\Icons\Heroicon::OutlinedBellSlash"
         :icon-alias="
@@ -44,7 +52,10 @@
         @if ($hasNotifications)
             <x-slot name="header">
                 <div>
-                    <h2 class="fi-modal-heading">
+                    <h2
+                        id="database-notifications.heading"
+                        class="fi-modal-heading"
+                    >
                         {{ __('filament-notifications::database.modal.heading') }}
 
                         @if ($unreadNotificationsCount)
@@ -72,17 +83,28 @@
                 </div>
             </x-slot>
 
-            @foreach ($notifications as $notification)
-                <div
-                    wire:key="{{ $notification->getKey() }}.database-notifications.ctn"
-                    @class([
-                        'fi-no-notification-read-ctn' => ! $notification->unread(),
-                        'fi-no-notification-unread-ctn' => $notification->unread(),
-                    ])
-                >
-                    {{ $this->getNotification($notification)->inline() }}
-                </div>
-            @endforeach
+            <div
+                aria-label="{{ __('filament-notifications::database.modal.heading') }}"
+                role="list"
+                class="fi-no-notifications"
+            >
+                @foreach ($notifications as $notification)
+                    <div
+                        role="listitem"
+                        wire:key="{{ $notification->getKey() }}.database-notifications.ctn"
+                        @class([
+                            'fi-no-notification-read-ctn' => ! $notification->unread(),
+                            'fi-no-notification-unread-ctn' => $notification->unread(),
+                        ])
+                    >
+                        @if ($notification->unread())
+                            <span class="fi-sr-only">{{ __('filament-notifications::database.modal.unread_label') }}</span>
+                        @endif
+
+                        {{ $this->getNotification($notification)->inline() }}
+                    </div>
+                @endforeach
+            </div>
 
             @if ($broadcastChannel = $this->getBroadcastChannel())
                 @script
